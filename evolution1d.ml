968c276c0bbb7b72b195959414a209d82164db75
Abhi Sarma <as3232@cornell.edu>
2021-04-22 19:02:03 -0400

    2D Free Particle Spectral

--- evolution1d.ml
+++ evolution1d.ml
@@ -115,7 +115,7 @@ let get_k (n : int) (d : domain) =
 let get_k2 (n : int) (d : domain) =
   List.map Complex.norm2 (get_k n d)
 
-module FreeParticleEvolutionSpectral : Evolution1D = struct
+module FreeParticleEvolutionSpectral1D : Evolution1D = struct
   (** AF: t represents the (discrete) fourier transform of a 
     wave function. *)
   type t = Complex.t list
@@ -188,7 +188,7 @@ let second_derivative (w : Complex.t array) (b : boundary_conditions)
     
   wxx
 
-module FreeParticleEvolutionEulers = struct
+module FreeParticleEvolutionEulers1D = struct
   (** AF: t represents the wave function as an array of complex numbers,
     the wave function evaluated at each point in the discretized domain. 
     In other words, if w : t, then w.(i) is the evaluation of w at the
@@ -201,7 +201,7 @@ module FreeParticleEvolutionEulers = struct
   let boundary_condition = [Periodic; Dirichlet; 
     Neumann (Complex.zero, Complex.zero)]
 
-  let normalize = FreeParticleEvolutionSpectral.normalize
+  let normalize = FreeParticleEvolutionSpectral1D.normalize
 
   let from_list vec = 
     normalize vec |> Array.of_list
@@ -255,7 +255,7 @@ let get_x2 (n : int) (d : domain) =
   let x = get_x n d in
   Array.map (fun y -> y*.y) x
 
-module HarmonicOscillatorEvolutionEulers = struct
+module HarmonicOscillatorEvolutionEulers1D = struct
 
   type t = Complex.t array
 
@@ -264,7 +264,7 @@ module HarmonicOscillatorEvolutionEulers = struct
   let boundary_condition = [Periodic; Dirichlet; 
     Neumann (Complex.zero, Complex.zero)]
 
-  let normalize = FreeParticleEvolutionSpectral.normalize
+  let normalize = FreeParticleEvolutionSpectral1D.normalize
 
   let from_list vec = 
     normalize vec |> Array.of_list
@@ -305,3 +305,157 @@ module HarmonicOscillatorEvolutionEulers = struct
     
     wnew
 end
+
+type domain2d = domain * domain
+
+module type Evolution2D = sig
+
+  (** Representation type of function *)
+  type t
+
+  (** List to represent the different allowed 
+    kinds of boundary conditions. *)
+  val boundary_condition : boundary_conditions list
+
+  (** Function to normalize a given vector representation
+    of a wave function *)
+  val normalize : (Complex.t list) list -> (Complex.t list) list
+
+  (** Function to convert from vector representation 
+    of wave function to representation type*)
+  val from_list :  (Complex.t list) list -> t
+
+  (** Function to convert from representation type
+    to vector representation of wave function*)
+  val to_list : t -> (Complex.t list) list
+
+  (** Function to convert from representation type
+    to vector representation of probability distribution
+    (Psi^dag * Psi) *)
+  val probabilities : t -> (float list) list
+
+  (** Function to evolve a wavefunction in time for a single timestep. 
+  Returns the internal representation of the wavefunction after one timestep*)
+  val step : t -> float -> boundary_conditions -> domain2d -> t
+
+  (** Function to evolve a wavefunction in time
+    for a given time (in seconds). Returns the internal
+    representation of the wavefunction after the last timestep.
+    Boolean argument tells function whether to print
+    each timestep in a file. *)
+  val evolve : t -> float -> boundary_conditions -> domain2d -> float -> bool -> t
+
+end
+
+(** [transpose mat] returns the matrix transpose of the 2d list mat *)
+let rec transpose mat = 
+  match mat with
+  | []             -> []
+  | []   :: xss    -> transpose xss
+  | (x::xs) :: xss ->
+    (x :: List.map List.hd xss) :: transpose (xs :: List.map List.tl xss)
+
+(** [col_map f mat] returns mat with each column of mat replaced with
+  the function f applied to that column. f therefore must take in an 'a list
+  and returns a 'b list of the same length. *)
+let rec col_map f mat = 
+  match mat with
+  |[] -> []
+  |h :: t -> f h :: col_map f t
+
+(** [row_map f mat] returns map with each row of mat replaced with
+  the function f applied to that column. f therefore must take in an 'a list
+  and returns a 'b list of the same length. *)
+let row_map f mat = 
+  mat |> transpose |> col_map f |> transpose
+
+(** Fast fourier transform of the 2d list mat, with each dimension having 
+    n elements.
+    Precondition: n is a power of 2 *)
+let re_fft n vec = fft vec n
+let re_ifft n vec = ifft vec n
+
+let fft2 mat n m = mat |> row_map (re_fft n) |> col_map (re_fft m)
+
+(** Inverse fast fourier transform of the 2d list mat, with each dimension 
+    having n elements.
+    Precondition: n is a power of 2 *)
+let ifft2 mat n m = 
+  mat |> List.rev |> col_map (re_ifft m) |> List.rev
+  |> transpose |> List.rev |> transpose |> row_map (re_ifft n) 
+  |> transpose |> List.rev |> transpose
+
+
+let get_k2_2d (n : int) (m : int) (d2 : domain2d) =
+  List.map2 (fun x y -> x +. y ) (get_k2 n (fst d2)) (get_k2 m (snd d2))
+
+module FreeParticleEvolutionSpectral2D : Evolution2D = struct
+  (** AF: t represents the 2D (discrete) fourier transform of a 
+    wave function. *)
+  type t = (Complex.t list) list
+  
+  let boundary_condition = [Periodic]
+
+  let norm1d vec = 
+    List.fold_left (fun acc x -> acc +. Complex.norm2 x) 0. vec |> sqrt
+
+  let div1d vec norm = 
+    List.map (fun x -> Complex.div x {re = norm; im = 0.}) vec
+  
+  let normalize mat = 
+    let rec norm mat =
+      match mat with
+      |[] -> 0.
+      |h :: t -> (norm1d h) +. (norm t)
+    in
+    let matnorm = norm mat in
+    let rec div mat = 
+      match mat with
+      |[] -> []
+      |h :: t -> (div1d h matnorm) :: (div t)
+    in
+    div mat
+  
+  (** [from_list mat] returns internal representation of mat, of type t.
+      Precondition: mat is a Complex.t list list of length n by m, where
+      n and m are both powers of 2.*)
+  let from_list mat = 
+    (mat |> normalize |> fft2) (List.length mat) (List.length (List.hd mat))
+
+  let to_list w =
+    (w |> ifft2) (List.length w) (List.length (List.hd w))
+
+  let probabilites1d vec =
+    vec |> List.map Complex.norm2
+  
+  let probabilities w = 
+    let mat = to_list w in
+    let rec probs mat =
+      match mat with
+      |[] -> []
+      |h :: t -> (probabilites1d h) :: (probs t)
+    in
+    probs mat
+
+  let step w tau b d2 = 
+    if b <> Periodic then 
+      raise (Invalid_argument "Illegal boundary condition.");
+    let n = List.length w in
+    let m = List.length (List.hd w) in
+    let k2 = get_k2_2d n m d2 in
+    let f w1d = 
+      List.map (fun x -> Complex.exp {Complex.re = 0.; im = -0.5 *. tau *. x}) k2
+      |> List.map2 Complex.mul w1d
+    in
+    col_map f w
+
+  let evolve w tau b d2 time print =
+    step w time b d2
+    (*TODO: Implement printing *)
+end
+
+(**
+module FreeParticleEvolutionEulers2D : Evolution2D = struct
+
+end
+*)
